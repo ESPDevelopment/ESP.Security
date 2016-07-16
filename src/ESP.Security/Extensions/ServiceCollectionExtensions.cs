3df7c81aa97bb3891aea37ff0475d5dd2e478e37
Rick Martinez <rickm@espdevelopment.com>
2016-07-16 08:19:39 -0700

    Iteration 1: Implemented JWT Bearer token

---
+++ src/ESP.Security/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using ESP.Security.Token;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ESP.Security.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        private const string TokenAudience = "http://espdevelopment.com";
+        private const string TokenIssuer = "http://espdevelopment.com";
+
+        public static IServiceCollection AddESPTokenKey(this IServiceCollection services, IConfigurationRoot configuration)
+        {
+            // Create security key
+            RsaSecurityKey key = RSAKeyUtils.GetDecodedKey(configuration["TokenAuth:Secret"]);
+
+            // Create token auth options
+            TokenAuthOptions tokenAuthOptions = new TokenAuthOptions()
+            {
+                Audience = TokenAudience,
+                Issuer = TokenIssuer,
+                SigningKey = key,
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256Signature)
+            };
+
+            // Add token auth instance
+            services.AddSingleton<TokenAuthOptions>(tokenAuthOptions);
+
+            // Return result
+            return services;
+        }
+    }
+}
